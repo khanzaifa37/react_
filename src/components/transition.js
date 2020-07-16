38c12f55465b2b039d6e595971a93bcb64f86e04
khanzaifa37 <khanzaifa37@gmail.com>
2020-07-16 20:33:56 +0530

    React Transitions

---
+++ src/components/transition.js
@@ -0,0 +1,54 @@
+import React ,{Component} from 'react';
+import {Transition} from 'react-transition-group';
+import '../css/App.css'
+
+
+class TransitionComp extends Component{
+    
+    state={
+        show:true
+    }
+    showDiv=()=>{
+        this.setState({
+            show:!this.state.show?true:false
+        })
+    }
+    
+    
+    render(){
+        return(
+            <div>
+                <Transition
+                    in={this.state.show}
+                    timeout={{
+                        enter:2000,
+                        exit:2000
+                    }}
+
+                    enter={true}
+                    exit={true}
+
+                    onEnter={(node) =>{
+                        // console.log('Enter');
+                    }} 
+                    onExit={(node) =>{
+                        // console.log('Exit');
+                    }} 
+                    >
+
+                        {state =>
+                            <div className={`square square-${state}`}>
+                                {`square square-${state}`}
+                            </div>
+                        }
+                    </Transition>
+                    <div className="showDiv"
+                            onClick={this.showDiv}>
+                        Show or Hide
+                    </div>
+            </div>
+        )
+   }
+}
+
+export default TransitionComp;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM, { render } from 'react-dom';
+import {BrowserRouter, Route,Link,NavLink,Switch } from 'react-router-dom';
+import './css/App.css'
+// Components
+import Home from './components/home'
+import TransitionComp from './components/transition';
+import CSS from './components/cssTransition';
+import Fade from './components/cssTransition';
+
+
+const App=()=>{
+    return (
+        <div>
+            <BrowserRouter>
+                <header>
+                    <NavLink to="/"><button>Home</button></NavLink>
+                    <NavLink to="/transition"><button>Transition</button></NavLink>
+                    <NavLink to="/css-transition"><button>CSS Transition</button></NavLink>
+                    <hr/>
+                </header>
+                <Switch>
+                    <Route path="/css-transition" component={Fade} />
+                    <Route path="/transition" component={TransitionComp} />
+                    <Route path="/" exact component={Home} />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    )
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App/>,document.getElementById("root")
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    
